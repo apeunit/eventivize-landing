494da00bc84ac0021c6e149a0561941ef77e44bd
Dario Aschero <dario.aschero@gmail.com>
2021-01-25 14:41:47 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -33,7 +33,7 @@ const Header = props => {
               <a href="./">About</a>
             </li>
             <li><a href="https://blog.apeunit.com/tag/eventivize/">Blog</a></li>
-            <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfIgzj57sQgA2RGiWqnL2l-yNyfQOZb8GJVXCpBTqXhBwFyOQ/viewform/">Join us</a></li>
+            <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfIgzj57sQgA2RGiWqnL2l-yNyfQOZb8GJVXCpBTqXhBwFyOQ/viewform">Join us</a></li>
 
           </ul>
         </Navbar>
